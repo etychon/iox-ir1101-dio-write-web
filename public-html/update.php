00d76aada3be99a9080b1086d82a1d059efdc9e0
etychon <etychon@gmail.com>
2020-08-19 19:06:29 +0200

    Added sanity check for update.php

--- public-html/update.php
+++ public-html/update.php
@@ -1,13 +1,18 @@
 <?php
  if(isset($_POST["insert"]))
  {
+   // get the data from the JS Ajax call on which port to trigger
    $myData = json_decode($_POST["insert"], true);
 
-   $exec = "echo out > /dev/dio-{$myData['num']}";
-   shell_exec($exec);
+   // $myData['num'] is the DIO port number (1-4)
+   if ((is_numeric($myData['num'])) && (is_numeric($myData['status'])))
+   { 
+     $exec = "echo out > /dev/dio-{$myData['num']}";
+     shell_exec($exec);
 
-   $exec = "echo {$myData['status']} > /dev/dio-{$myData['num']}";
-   shell_exec($exec);
+     $exec = "echo {$myData['status']} > /dev/dio-{$myData['num']}";
+     shell_exec($exec);
+   }
  }
 ?>
   
